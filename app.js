0eb5d6cfddec8856e2d0db0e4d0eeac24c9e7657
Miguel Eraso <96079469+migueldavideraso@users.noreply.github.com>
2024-03-07 00:39:51 -0500

    Change path

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", async (req, res) => {
+app.get("/create-backup", async (req, res) => {
 
   const timeStart = new Date().toLocaleString()
 
   console.log('timeStart', timeStart)
 
-  const minutes = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+  const minutes = [ 0, 1/* , 2, 3, 4, 5, 6, 7, 8, 9, 10  */]
 
   await Promise.all(minutes.map(async (min) => {
     const time = getMinutes(min)
