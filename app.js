05342f7ecdaed45dc9a80b31b78ef807742f4f5a
Miguel Eraso <96079469+migueldavideraso@users.noreply.github.com>
2024-03-07 00:16:07 -0500

    Change to multiples timeouts

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ app.get("/", async (req, res) => {
 
   const timeStart = new Date().toLocaleString()
 
-  console.log(timeStart)
+  console.log('timeStart', timeStart)
 
-  await new Promise((resolve, reject) => {
-    setTimeout(() => resolve(), getMinutes(10))
-  })
+  const minutes = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+  await Promise.all(minutes.map(async (min) => {
+    const time = getMinutes(min)
+    await timeOut(time)
+
+    console.log('time: ' + time)
+    console.log('date: ' + new Date().toLocaleString())
+  }))
 
   const timeEnd = new Date().toLocaleString()
 
-  console.log(timeEnd)
+  console.log('timeEnd', timeEnd)
 
   res.json({
     status: 'completed',
@@ -27,6 +33,15 @@ app.get("/", async (req, res) => {
 function getMinutes (minutes) {
   return 1_000 * 60 * minutes
 }
+
+
+function timeOut (time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), time)
+  })
+}
+
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
